2948469c8a907175dd180616d2ee9864aa68ee32
Alexandr Suslov <im.cynic@gmail.com>
2015-03-25 08:54:42 +0500

    ADC  IRQ + 128*64 OLED Display

--- _7.ino
+++ _7.ino
@@ -1,3 +1,11 @@
+#include <Adafruit_GFX.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_SSD1306.h>
+
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+
 // светодиодная шкала подключена к группе пинов расположенных
 // подряд. Даём понятные имена первому и последнему пинам
 #define FIRST_LED_PIN  2
@@ -9,43 +17,56 @@ void adc_init(void)
 { 
 // &= ~ обнуление бита
 // |= присвоение биту 1
-ADCSRA &= ~ (1 << ADEN); // отключаем ацп
-DIDR0 |= (1 << ADC0D); // выбираем вход а0
-ADMUX &= ~ (_BV(MUX3))|(_BV(MUX2))|(_BV(MUX1))|(_BV(MUX0));
-ADMUX |= _BV(REFS0);
-ADCSRA |= (ADPS2<<1)|(ADPS1<<1)|(ADPS0<<1); //делитель частоты 128
-ADCSRA |= (1<<ADEN)|(1<<ADSC)|(1<<ADIE);  // включаем ацп / запускаем одиночное преобразование / разрешаем прерывание после завершения прерывания
+ADCSRA &= ~ _BV(ADEN); // отключаем ацп
+DIDR0  |=   _BV(ADC0D); // выбираем вход а0
+ADMUX  &= ~ (_BV(MUX3))|(_BV(MUX2))|(_BV(MUX1))|(_BV(MUX0));
+ADMUX  |=   _BV(REFS0);
+ADCSRA |=   (_BV(ADPS2))|(_BV(ADPS1))|(_BV(ADPS0)); //делитель частоты 128
+ADCSRA |=   (_BV(ADEN))|(_BV(ADSC))|(_BV(ADIE));  // включаем ацп / запускаем одиночное преобразование / разрешаем прерывание после завершения преобразования
 }
 
 ISR(ADC_vect)
 {
  brightness = ADC; // отсылаем данные
- ADCSRA |= (1<<ADSC); //запускаем новое одиночное преобразование
+ ADCSRA |= _BV(ADSC); //запускаем новое одиночное преобразование
  }
 
 void setup()
 {
   // в шкале 10 светодиодов. 
+ Serial.begin(115200);
+ display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+ display.display();
  for (int pin = FIRST_LED_PIN; pin <= LAST_LED_PIN; ++pin)
    pinMode(pin, OUTPUT); 
-// Serial.begin(115200);
  adc_init();
  sei();
 }
  
 void loop()
 {
+int result = ((brightness) / 100);
 
-
-int result = (brightness / 100);
-
-
-for (int pin = FIRST_LED_PIN; pin <= result; ++pin) 
+for (int pin = FIRST_LED_PIN; pin <= result+1; ++pin) 
    digitalWrite(pin, HIGH);
 delay(100);
-for (int pin = FIRST_LED_PIN; pin <= result; ++pin) 
+for (int pin = FIRST_LED_PIN; pin <= result+1; ++pin) 
    digitalWrite(pin, LOW); 
-
-  
+   
+display.clearDisplay();
+display.setTextColor(WHITE);
+display.setTextSize(1);
+display.setCursor(1,5);
+display.println("LED's: ");
+display.setTextSize(2);
+display.setCursor(100,2);
+display.println(result);
+display.setTextSize(1);
+display.setCursor(1,39);
+display.println("Brightness: ");
+display.setTextSize(3);
+display.setCursor(70,30);
+display.println(brightness);
+display.display();
 }
 
